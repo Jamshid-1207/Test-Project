06870fe1c370f2e3d02ec106ec9506c3f2c6ca03
Jamshidbke <jama25jama25@mail.ru>
2026-03-04 16:10:24 +0500

    first commit on dev

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ProductsPage } from "@/pages/products/ProductsPage";
+import "@/index.css";
+function App() {
+  return (
+    <>
+      <ProductsPage />
+    </>
+  );
+}
+
+export default App;
